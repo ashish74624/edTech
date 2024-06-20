dced4d61230f0d709ad03c0c9f31d7b923801120
Ashish <ashish74624@gmail.com>
2024-06-20 22:40:29 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started on port : ${process.env.PORT}`);
+})
