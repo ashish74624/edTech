4cbf51acc3988e50fca7549818bd2c20c55e6272
Ashish <ashish74624@gmail.com>
2024-06-26 21:48:54 +0530

    Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
+import NavBar from "./components/Navbar";
 import { Meteors } from "./components/ui/meteors";
 
 export default function App() {
   return (
-    <main className="h-screen w-screen bg-black relative">
+    <main className="h-screen w-screen bg-black relative overflow-hidden">
+      <NavBar/>
       <Meteors number={20}/>
-      <section className="w-full h-full flex justify-center items-center">
+      <section className="w-full flex justify-center items-center">
         <h1 className="text-white text-7xl text-center">
-          Advance your Career <br /> in a Digitized World
+          Advance your Career <br /> in a Digitized World.
         </h1>
       </section>
     </main>
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+
+
+export default function NavBar() {
+  return (
+    <nav className="w-screen bg-transparent py-4 shadow-md border-b border-gray-100 text-white flex justify-between px-8">
+      <div>
+        <p>LOGO</p>
+      </div>
+      <div className="flex w-max gap-8 text-lg">
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Contact Us</p>
+      </div>
+    </nav>
+  )
+}
