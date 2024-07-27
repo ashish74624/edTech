0ede134b842ab95f3e53e4ea8724f93fd018482a
Ashish <ashish74624@gmail.com>
2024-07-27 23:50:35 +0530

    subscription side

--- client/src/components/Home/StudentSection.tsx
+++ client/src/components/Home/StudentSection.tsx
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react"
 import { Course } from "../Types/types";
 import { Link } from "react-router-dom";
 import useAuth from "@/hooks/useAuth";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
 
 const backend = import.meta.env.VITE_BACKEND;
 
@@ -28,7 +36,6 @@ export default function StudentSection() {
                 setCourses(data.courses);
             }
         }
-        void getCourseRecomendation();
         
         const mySubscriptions = async () => {
             const res = await fetch(`${backend}/api/student/mySubscriptions/${userData?._id}`);
@@ -37,37 +44,74 @@ export default function StudentSection() {
                 setSubscriptions(data.subscriptions);
             }
         } 
-        void mySubscriptions();
+        if(userData){
+
+            void getCourseRecomendation();
+            void mySubscriptions();
+        }
     },[userData]);
 
   return (
     <section className="text-white">
       <div>
-        <p>All Coures</p>
+        {/* <p>All Coures</p> */}
         {
             courses && 
-            courses.map((item)=>(
-                <Link key={item._id} to={`/course/${item._id}`}>
-                <div>
-                    <img className="w-40 h-40" src={`https://res.cloudinary.com/dknsgexk8/image/upload/v1721739667/${item.thumbnail}`}/>
+            <>
+            <Carousel plugins={[ Autoplay({ delay: 2000 }) ]} className="w-[80vw] mx-auto mt-4">
+                <CarouselContent>
+                {courses.slice(0,5).map((item)=>(
+                    <Link key={item._id} to={`/course/${item._id}`}>
+                    <CarouselItem className="w-[80vw] h-96">
+                        <img className="w-[80vw] h-96 rounded-md border-white border" src={`https://res.cloudinary.com/dknsgexk8/image/upload/v1721739667/${item.thumbnail}`}/>
+                    </CarouselItem>
+                    </Link>
+                ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+            <div className="mt-4 w-[80vw] mx-auto">
+                <h2 className="text-4xl mb-4">All Courses</h2>
+                <div className="grid-cols-8 md:grid-cols-2 xl:grid-cols-3 gap-4 w-max mx-auto grid">
+
+                {
+                    courses.map((item)=>(
+                        <Link key={item._id} to={`/course/${item._id}`}>
+                            <div className="bg-black w-96 max-h-72 h-max rounded-md overflow-hidden border border-white" >
+                                <img className="w-full h-56 rounded-md" src={`https://res.cloudinary.com/dknsgexk8/image/upload/v1721739667/${item.thumbnail}`} alt={item.title}/>
+                                <p className="m-4">
+                                    {item.title}
+                                </p>
+                            </div>
+                            </Link>
+                    ))
+                }
                 </div>
-                </Link>
-            ))
+            </div>
+            </>
+
         }
       </div>
-      <div>
-        <p>Your subscriptions</p>
+      <div className="w-[80vw] mx-auto mt-8">
+        <p className="text-4xl">Your subscriptions</p>
+        <div className="grid place-content-center grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 w-max mx-auto">
+
         {
             subscriptions 
             ?
             subscriptions.map((item,i)=>(
                 <Link to={`/teacher/${item._id}`} key={i}>
-                    {item.email}
+                    <div className="bg-black border border-white px-8 py-2 rounded-md">
+                        <p className="text-xl">{item.firstName} {" "} {item.lastName}</p>
+                        <p>{item.email}</p>
+                    </div>
                 </Link>
             ))
             :
             <div>You currently have no subscriptions</div>
         }
+        </div>
       </div>
     </section>
   )
