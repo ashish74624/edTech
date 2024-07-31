fe9ef3ae7640a87762df842e0b866c5ac4f65c95
Ashish <ashish74624@gmail.com>
2024-07-31 11:54:28 +0530

    Landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
-import NavBar from "./components/NavbarLanding";
 import { Meteors } from "./components/ui/meteors";
 import Button from "./components/Global/Button";
 
 export default function App() {
   return (
-    <main className="h-screen w-screen bg-zinc-900 relative overflow-hidden">
-      <NavBar/>
+    <main className="h-screen w-screen flex justify-center items-center bg-zinc-900 relative overflow-hidden">
       <Meteors number={20}/>
-      <section className="w-full h-max my-8">
-        <h1 className="text-white text-3xl md:text-5xl lg:text-7xl mx-auto w-full lg:w-[45%]">
-          Advance your Career <br /> in a Digitized World.
+      <section className="w-full h-max text-center">
+        <h1 className="text-teal  text-3xl md:text-5xl lg:text-8xl font-Lobster mx-auto w-full lg:w-[45%]">
+          InspireEdTech
         </h1>
+        <h2 className="text-white text-3xl md:text-5xl mx-auto w-full lg:w-[45%]">
+          Advance your Career in a Digitized World.
+        </h2>
         <p className="text-gray-200 text-sm md:text-base mx-auto mt-4 w-full lg:w-[45%]">
           We provide you with unrestricted access to greatest courses from the top teachers, allowing you to learn from countless lessons in a range of topics.
         </p>
@@ -19,7 +20,6 @@ export default function App() {
           <Link to={'/login'}>
             <Button variant='large' text="Get Started"/>
           </Link>
-          {/* <Button text="Upload Course"/> */}
         </div>
       </section>
     </main>
