04d5e2df4381d3868e18391e1db4af23d18ace85
Ashish <ashish74624@gmail.com>
2024-07-31 11:56:43 +0530

    Sidebg done

--- client/src/components/Auth/SideBg.tsx
+++ client/src/components/Auth/SideBg.tsx
@@ -8,7 +8,7 @@ export default function SideBg({heading,text}:Props) {
     <section className="hidden xl:block relative h-screen w-[50vw]">
         <img src="/teaching.jpg" alt="teaching_img" className='w-full h-full' />
         <div className='w-full h-full bg-black/60 absolute inset-0 flex flex-col justify-center items-center text-white gap-2'>
-        <h1 className="text-teal text-5xl">LOGO</h1>
+        <h1 className="text-teal font-Lobster text-5xl">InspireEdTech</h1>
           <h3 className="text-3xl">{heading}</h3>
           <h3 className="text-xl">{text}</h3>
         </div>
