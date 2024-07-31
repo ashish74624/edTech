339cf44fcd647eecfb6d7ab6bbc775c80cbd3dea
Ashish <ashish74624@gmail.com>
2024-07-31 11:26:20 +0530

    Teacher page

--- client/src/pages/Teacher.tsx
+++ client/src/pages/Teacher.tsx
@@ -47,19 +47,25 @@ export default function Teacher() {
   return (
     <section className='page-class text-white'>
       <Navbar/>
-      <p>Teacher courses</p>
+      <p className="text-4xl mt-4 mx-2">Teacher courses</p>
       {
         teacher 
         ?
         <div>
-          <p>{teacher.firstName} {' '} {teacher.lastName}</p>
+          <p className="text-3xl w-max mx-auto">{teacher.firstName} {' '} {teacher.lastName}</p>
+          <div className="grid grid-cols-1 mt-8 md:grid-cols-2 xl:grid-cols-3 gap-4 w-max mx-auto">
+
           {teacher.courses.map((item)=>(
             <Link to={`/course/${item._id}`}>
-              <div>
-                <img className="w-40 h-40" src={`https://res.cloudinary.com/dknsgexk8/image/upload/v1721739667/${item.thumbnail}`} alt={item.title}/>
+              <div className="bg-black w-96 max-h-72 h-max rounded-md overflow-hidden border border-white">
+                <img className="w-full h-56 rounded-md" src={`https://res.cloudinary.com/dknsgexk8/image/upload/v1721739667/${item.thumbnail}`} alt={item.title}/>
+                <p className="m-4">
+                  {item.title}
+                </p>
               </div>
             </Link>
           ))}
+          </div>
         </div>
         :
         <p>This Teacher has no courses currently</p>
